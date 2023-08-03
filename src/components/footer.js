a33051e898533c694e01cbb0733edc36a7f35946
RodCato <code@eliment.org>
2023-08-02 22:27:15 -0500

    Added gatsby logo to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
 
 const Footer = () => {
   return (
     <div>
       <footer class="footer">
         <a href="https://github.com/RodCato" class="link footer__link">
-          Created By CRod with 🫶🏽 || &copy; 2023 || 🎟️ Thanks for Visiting! 🎬
+          Created By CRod || With 🫶🏽 and
+          <StaticImage
+            src="../images/Gatsby_Monogram.png"
+            alt="Gatsby Logo"
+            className="object-scale-down h-4 w-4 mt-1 ml-2 mr-1 inline-block"
+          />
+          || &copy; 2023 || 🎟️ Thanks for Visiting! 🎬
         </a>
       </footer>
     </div>
