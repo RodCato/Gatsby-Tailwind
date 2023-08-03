e3df526bc47af04e8569281968591b74a3852f7f
RodCato <code@eliment.org>
2023-08-02 21:04:51 -0500

    Updates

--- component---src-pages-page-2-js-41ee92c6ebf09584a533.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t),a.d(t,{Head:function(){return u}});var l=a(7294),n=a(1883),r=a(7395),c=a(9357);const u=()=>l.createElement(c.Z,{title:"Page two"});t.default=()=>l.createElement(r.Z,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-41ee92c6ebf09584a533.js.map
