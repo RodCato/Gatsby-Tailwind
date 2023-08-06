93024181ade7d608640a3e623c9d33f42e95bf55
RodCato <code@eliment.org>
2023-08-05 21:09:16 -0500

    testing new form submission process and new formsucces page with redirect

---
+++ src/pages/formsuccess.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react"
+import { Link } from "gatsby"
+
+const FormSuccess = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Redirect to the main page after 5 seconds
+      window.location.href = "/"
+    }, 5000) // 5000 milliseconds = 5 seconds
+
+    return () => clearTimeout(timer) // Clear the timer if the component unmounts
+  }, [])
+
+  return (
+    <div className="text-center">
+      <h2>Message Sent Successfully!</h2>
+      <p>Thank you for your submission. You will receive a reply soon.</p>
+      <p>Redirecting to the main page in 5 seconds...</p>
+      <Link to="/">Go back to the main page</Link>
+    </div>
+  )
+}
+
+export default FormSuccess
