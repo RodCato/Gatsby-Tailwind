93e15da767297ed3e10aee2d726eb7302afaf870
RodCato <code@eliment.org>
2023-08-05 21:25:03 -0500

    formsuccess redirect link update with timed redirect fix

--- src/pages/formsuccess.js
+++ src/pages/formsuccess.js
@@ -5,7 +5,7 @@ const FormSuccess = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       // Redirect to the main page after 5 seconds
-      window.location.href = "/"
+      window.location.href = "https://crod.co/gp"
     }, 5000) // 5000 milliseconds = 5 seconds
 
     return () => clearTimeout(timer) // Clear the timer if the component unmounts
@@ -16,7 +16,7 @@ const FormSuccess = () => {
       <h2>Message Sent Successfully!</h2>
       <p>Thank you for your submission. You will receive a reply soon.</p>
       <p>Redirecting to the main page in 5 seconds...</p>
-      <Link to="https://crod.co/gp">Go back to the main page</Link>
+      <Link to="https://crod.co/gp">Go back to the main page 🔗</Link>
     </div>
   )
 }
