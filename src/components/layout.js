3f62dac3e6595c7a28fd13ed0faef202ec456a05
RodCato <code@eliment.org>
2023-08-13 00:09:18 -0500

    added meta tags to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-// import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet-async"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
@@ -15,40 +14,42 @@ import Contact from "./contact"
 import previewImage from "../images/preview.png"
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query pageQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" })
   }
 
   return (
     <>
-      <Helmet>
-        <meta property="og:image" content={previewImage} />
-        <meta property="og:title" content="👑CRodDev's Portfolio" />
-        <meta
-          property="og:description"
-          content="I'm a Software Engineer with a passion for learning new things, creativity and innovation. I love to build things that make a difference. I hope you enjoy my work!"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin%20Sans:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
+      <Helmet
+        title="👑CRod's Portfolio"
+        meta={[
+          {
+            property: `og:title`,
+            content: "👑CRodDev's Portfolio",
+          },
+          {
+            property: `og:description`,
+            content:
+              "I'm a Software Engineer with a passion for learning new things, creativity and innovation. I love to build things that make a difference. I hope you enjoy my work!",
+          },
+          {
+            property: `og:image`,
+            content: previewImage,
+          },
+        ]}
+      />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Josefin%20Sans:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
+
       <Header />
       <main>
         <About />
