de5a9ae09214a6cc9652ec534f8aff8e4ceadb0f
RodCato <code@eliment.org>
2023-10-23 22:30:31 -0500

    update blogfeed component

--- src/components/blogfeed.js
+++ src/components/blogfeed.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 
-const WordPressJsonFeed = () => {
+const BlogFeed = () => {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
@@ -31,4 +31,4 @@ const WordPressJsonFeed = () => {
   )
 }
 
-export default WordPressJsonFeed
+export default BlogFeed
